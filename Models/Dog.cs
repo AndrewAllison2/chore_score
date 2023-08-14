1ec09edc489172ae269b14232fe8703429e58fcd
Andrew Allison <andrewallison5211@gmail.com>
2023-08-14 15:04:26 -0600

    new model

---
+++ Models/Dog.cs
@@ -0,0 +1,14 @@
+
+namespace chore_score.Models;
+
+    public class Dog
+    {
+        public string Name{ get; set; }
+        public int Age { get; set; }
+        public bool HasShortHair { get; set; }
+        public bool Drools { get; set; }
+        public string FavoriteToy { get; set; }
+    }
+    
+        
+    
